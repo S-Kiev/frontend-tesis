3756d31adeff7399ff1e54f86d71649f4204e7a9
Eugenia_finozzi <eugebelen@hotmail.es>
2023-12-28 06:30:52 -0300

    fix navar position + add users view header

--- src/screens/Users/Users.tsx
+++ src/screens/Users/Users.tsx
@@ -1,9 +1,94 @@
 import { FC } from 'react';
+import styles from './Users.module.scss';
+import { useNavigate } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 interface UsersProps {}
 
 const Users: FC<UsersProps> = () => {
-  return <div>Users</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div>
+          <h2 className={styles.headline}>Usuarios</h2>
+          <p>Listado de todos los usuarios existentes en el sistema</p>
+        </div>
+        <Button variant="success" onClick={() => navigate('/app/users/create')} className="d-none d-lg-block">
+          + Crear nuevo usuario
+        </Button>
+      </div>
+      <Button variant="success" onClick={() => navigate('/app/users/create')} className="d-lg-none mt-3">
+        + Crear nuevo usuario
+      </Button>
+
+      {/*data?.data.length === 0 && !loading && !filterActive ? (
+  <div className={styles.errorFilters}>
+    <img src={ProfileIcon} alt="Usuarios" />
+    <h3 className="mt-3">Aún no existen usuarios</h3>
+  </div>
+) : (
+  <div>
+    {!error ? (
+      <div>
+        <div className={styles.filters}>
+          <div className={styles.inputs}>
+            <CustomInputFilter
+              setter={setSearch}
+              value={search}
+              placeHolder="Buscar por nombre, email o ID"
+            />
+            <CustomDatePickerRange
+              label=""
+              startDate={startDate}
+              setStartDate={setStartDate}
+              endDate={endDate}
+              setEndDate={setEndDate}
+              customPlaceholder={startDate ? null : 'Seleccionar fecha'}
+              setFilterByDate={setFilterByDate}
+              dateFormat={'dd-MMM - dd-MMM, yy'}
+              maxDate={new Date()}
+              name="date"
+            />
+          </div>
+          <Button
+            onClick={toggleFilters}
+            style={{ alignSelf: 'flex-end' }}
+            size="medium"
+            type="secondary"
+            iconPosition="left"
+            Icon={FilterIcon}
+            text="Filtrar"
+          />
+          <FiltersModalUsers
+            isOpen={showFilters}
+            toggleModal={toggleFilters}
+            setFilters={setFilters}
+            filters={filters}
+          />
+        </div>
+      </div>
+    ) : null*/}
+      {/*error ? <LoadErrorModal toggleModal={toggleErrorModal} isOpen={showErrorModal} /> : null*/}
+      {/*loading ? <Loader /> : null*/}
+
+      {/*!loading && !error && data && data?.data.length !== 0 ? (
+      <UsersTable usersRequests={data?.data} />
+    ) : (
+      !error &&
+      !loading &&
+      filterActive && (
+        <div className={styles.errorFilters}>
+          <img src={SearchIcon} alt="lupas" />
+          <h3 className="mt-3">No encontramos resultados</h3>
+          <p className="mb-3">Puedes intentarlo nuevamente modificando los filtros seleccionados</p>
+          <Button size="large" text="Ver filtros" type="secondary" onClick={toggleFilters} />
+        </div>
+      )
+      )*/}
+    </div>
+  );
 };
 
 export default Users;
