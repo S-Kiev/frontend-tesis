5118b8d8da747ef151b912c5ecd246e6b8e2ae48
Eugenia_finozzi <eugebelen@hotmail.es>
2024-01-23 13:02:51 -0300

    add ProgressBar and title sections

--- src/components/customerCreateForm/customerCreateForm.tsx
+++ src/components/customerCreateForm/customerCreateForm.tsx
@@ -4,6 +4,9 @@ import PersonalInfo from './stepsForm/personalInfo';
 import ContactInfo from './stepsForm/contactInfo';
 import MedicalInfo from './stepsForm/medicalInfo';
 import InformedConsent from './stepsForm/informedConsent';
+import { ProgressBar } from 'react-bootstrap';
+import styles from './customerCreateForm.module.scss';
+import { Icon1CircleFill, Icon2CircleFill, Icon3CircleFill, Icon4CircleFill } from 'react-bootstrap-icons';
 
 interface CustomerCreateFormProps {}
 
@@ -41,13 +44,49 @@ const CustomerCreateForm: FC<CustomerCreateFormProps> = () => {
   console.log(customerData);
   switch (step) {
     case 1:
-      return <PersonalInfo setStep={setStep} customerData={customerData} setCustomerData={setCustomerData} />;
+      return (
+        <>
+          <div className="d-flex align-items-center mb-2">
+            <Icon1CircleFill size={32} className="me-2" color="rgba(8, 135, 93, 1)" />
+            <h3 className={styles.title}>Información personal</h3>
+          </div>
+          <ProgressBar variant="success" now={25} striped />
+          <PersonalInfo setStep={setStep} customerData={customerData} setCustomerData={setCustomerData} />
+        </>
+      );
     case 2:
-      return <ContactInfo setStep={setStep} customerData={customerData} setCustomerData={setCustomerData} />;
+      return (
+        <>
+          <div className="d-flex align-items-center mb-2">
+            <Icon2CircleFill size={32} className="me-2" color="rgba(8, 135, 93, 1)" />
+            <h3 className={styles.title}>Información de contacto</h3>
+          </div>
+          <ProgressBar variant="success" now={50} striped />
+          <ContactInfo setStep={setStep} customerData={customerData} setCustomerData={setCustomerData} />
+        </>
+      );
     case 3:
-      return <MedicalInfo setStep={setStep} customerData={customerData} setCustomerData={setCustomerData} />;
+      return (
+        <>
+          <div className="d-flex align-items-center mb-2">
+            <Icon3CircleFill size={32} className="me-2" color="rgba(8, 135, 93, 1)" />
+            <h3 className={styles.title}>Información médica</h3>
+          </div>
+          <ProgressBar variant="success" now={75} striped />
+          <MedicalInfo setStep={setStep} customerData={customerData} setCustomerData={setCustomerData} />
+        </>
+      );
     case 4:
-      return <InformedConsent setStep={setStep} customerData={customerData} setCustomerData={setCustomerData} />;
+      return (
+        <>
+          <div className="d-flex align-items-center mb-2">
+            <Icon4CircleFill size={32} className="me-2" color="rgba(8, 135, 93, 1)" />
+            <h3 className={styles.title}>Consentimiento informado</h3>
+          </div>
+          <ProgressBar variant="success" now={100} striped />
+          <InformedConsent setStep={setStep} customerData={customerData} setCustomerData={setCustomerData} />
+        </>
+      );
     default:
       return <div>Error</div>;
   }
